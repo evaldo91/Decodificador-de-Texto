1c9616cb408a0b994fb4b3e0da13c581c6a4f0f4
Evaldo Butzke <evaldo.but@live.com>
2024-01-23 20:37:15 -0300

    atulização do codificador incluindo metro de validação do texto na propria função

--- js/codificador.js
+++ js/codificador.js
@@ -1,38 +1,64 @@
 
 //criptografia
 function criptografia(texto){
-    let sepadorTexto = texto.split('');
-
-    sepadorTexto.forEach(function (caracter, i){
-        if (caracter == 'e') {
-            sepadorTexto[i] = 'enter';
-        } else if (caracter == 'i') {
-            sepadorTexto[i] = 'ines';
-        } else if (caracter == 'a') {
-            sepadorTexto[i] = 'ai';
-        } else if (caracter == 'o') {
-            sepadorTexto[i] = 'ober';
-        } else if (caracter == 'u') {
-            sepadorTexto[i] = 'u';
+    let parametros = /[A-Z-À-Ú-à-ú]/;
+    if(parametros.test(texto)==false){
+        let sepadorTexto = texto.split('');
+        sepadorTexto.forEach(function (caracter, i){
+            if (caracter == 'e') {
+                sepadorTexto[i] = 'enter';
+            } else if (caracter == 'i') {
+                sepadorTexto[i] = 'ines';
+            } else if (caracter == 'a') {
+                sepadorTexto[i] = 'ai';
+            } else if (caracter == 'o') {
+                sepadorTexto[i] = 'ober';
+            } else if (caracter == 'u') {
+                sepadorTexto[i] = 'u';
+            }
         }
-
-    });
-    mostraRespota('p' , sepadorTexto.join(''));
-
-
+        );
+            mostraRespota('p' , sepadorTexto.join(''));
+        }else{
+            mostraRespota('p' , 'Apenas letras minúsculas e sem acento!')
+        }
 }
 // descroptografia
-
 function descriptografia(texto){
-    let descriptografia = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
-for(let i =0; i <descriptografia.length; i++){
-    if(texto.includes(descriptografia[i][1])){
-        texto = texto.replaceAll(descriptografia[i][1], descriptografia[i][0]);
+    let parametros = /[A-Z-À-Ú-à-ú]/;
+    if(parametros.test(texto)==false){
+        let descriptografia = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+        for(let i =0; i <descriptografia.length; i++){
+            if(texto.includes(descriptografia[i][1])){
+                texto = texto.replaceAll(descriptografia[i][1], descriptografia[i][0]);
+            }
+            mostraRespota('p' , texto);
+        }
+        
+        
+        
+    }else{
+        mostraRespota('p' , 'Apenas letras minúsculas e sem acento!')
     }
-}
-mostraRespota('p' , texto);
 
 }
+function btnCriptografar(){
+    let texto = document.querySelector('input').value;
+    criptografia(texto);
+
+    
+}
+
+function btnDescriptografar(){
+    let texto = document.querySelector('input').value;
+    descriptografia(texto);
+
+    
+}
+
+   
+
+
 
 
 function mostraRespota (tag, texto){
